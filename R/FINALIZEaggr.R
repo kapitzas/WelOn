00b7d29a7e9e1fad9d7dff63efa151ed7a0d0cff
kapitzas@student.unimelb.edu.au <kapitzas@6300D-132148-M.local>
2019-02-13 10:55:01 +1100

    changed documentation to matrix

--- R/FINALIZEaggr.R
+++ R/FINALIZEaggr.R
@@ -4,16 +4,17 @@
 #' @param aggr A matrix containing the existing, final aggregate statistics. Should be output of \code{UPDATEaggr}: \code{n}, \code{mean}, \code{M2}.
 #' @return A matrix with 1 row and 3 columns: Current size \code{n}, current mean \code{mean} and current unbiased variance, \code{var}.
 #' @examples
-#' #' seq <- c(1:10)
-#' m <- INIrun(seq[1])
-#' for (i in 2:10){
-#' m <- updatestats(m, seq[i])
+#' seq <- matrix(sample(1:100, 100), ncol = 5)
+#' m <- INIaggr(seq[1,])
+#' for (i in 2:20){
+#' m <- UPDATEaggr(m, seq[i,])
 #' }
 #'
-#' mfin <- finalize(m)
-#' all(mfin[1] == length(seq),
-#' mfin[2] == mean(seq),
-#' mfin[3] == var(seq)
+#' mfin <- FINALIZEaggr(m)
+#' all(
+#' all.equal(mfin[1,1], nrow(seq)),
+#' all.equal(mfin[,2], colMeans(seq)),
+#' all.equal(mfin[,3], apply(seq, 2, FUN = "var"))
 #' )
 #'
 #' @author Simon Kapitza \email{kapitzas@student.unimelb.edu.au}
--- R/UPDATEaggr.R
+++ R/UPDATEaggr.R
@@ -2,12 +2,12 @@
 #'
 #' This function updates n, mean and M2 when a new value is added to the sequence.
 #' @param aggr A matrix containing the existing aggregate statistics. Usually output of \code{INIaggr}: \code{n}, \code{mean}, \code{M2}.
-#' @return A matrix with 1 row and 3 columns: Current size \code{n}, current mean \code{mean} and current M2, \code{M2}, which is the average distance previous values to \code{mean}.
+#' @return A matrix with \code{nrow(aggr)} rows and 3 columns: Current size \code{n}, current mean \code{mean} and current M2, \code{M2}, which is the average distance previous values to \code{mean}.
 #' @examples
-#' #' seq <- c(1:10)
-#' m <- INIrun(seq[1])
-#' for (i in 2:10){
-#' m <- updatestats(m, seq[i])
+#' seq <- matrix(sample(1:100, 100), ncol = 5)
+#' m <- INIaggr(seq[1,])
+#' for (i in 2:20){
+#' m <- UPDATEaggr(m, seq[i,])
 #' }
 
 #' @author Simon Kapitza \email{kapitzas@student.unimelb.edu.au}
