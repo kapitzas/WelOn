4b861d31dfcda43775f69f8abc5f1c325b17bf1d
kapitzas@student.unimelb.edu.au <kapitzas@6300D-132148-M.local>
2019-02-13 12:12:33 +1100

    some changes to doc

--- R/INIaggr.R
+++ R/INIaggr.R
@@ -1,7 +1,7 @@
 #' Initiate a sequence of values to be updated online.
 #'
 #' This function initiates the output matrix for Welford's Online algorithm.
-#' @param vec A numeric value. This is the first value of a sequence.
+#' @param vec A single value or numeric vector. First sample of a sequence of samples.
 #' @return A matrix with \code{length(vec)} rows and 3 columns: Sample size \code{n = 1}, current mean \code{mean[i] = vec[i]} and current M2, \code{M2 = 0}, which is the average distance to \code{vec[i]} to \code{mean[i]}, so initially 0.
 #' @examples
 #' seq <- matrix(sample(1:100, 100), ncol = 5)
